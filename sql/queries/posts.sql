4d4c1b96f30e01acf9eba673d94df7509ae77fd3
Abdelrahman Amr <boody2205@gmail.com>
2026-05-01 12:08:41 +0300

    feat(db): add posts table
    - add posts table migrations in sql/schema/
    - the posts table will save the posts fetched from feeds
    - add a query to insert a post into the posts table
    - add a query to get a variable amount of the latest posts for the user
    - generate the code to handle the queries

---
+++ sql/queries/posts.sql
@@ -0,0 +1,9 @@
+-- name: CreatePost :one
+INSERT INTO posts (id, created_at, updated_at, title, url, description, published_at, feed_id)
+VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+RETURNING *;
+
+-- name: GatePostsForUser :many
+SELECT * FROM posts
+ORDER BY published_at
+LIMIT $1;
---
+++ sql/schema/005_posts.sql
@@ -0,0 +1,15 @@
+-- +goose Up
+-- Create a new table 'posts' with a primary key and columns
+CREATE TABLE posts (
+    id UUID PRIMARY KEY,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    url TEXT NOT NULL,
+    description TEXT NOT NULL,
+    published_at TIMESTAMP NOT NULL, -- the publish time of the post
+    feed_id UUID NOT NULL REFERENCES feeds(id)
+);
+
+-- +goose Down
+DROP TABLE posts;
